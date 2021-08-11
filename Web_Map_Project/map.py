11beb9d02549652e5e477b49db82feeca2fc629f
justin-sayah <jsayah@bu.edu>
2021-08-11 00:13:27 -0400

    work on map project

--- Web_Map_Project/map.py
+++ Web_Map_Project/map.py
@@ -1,6 +1,11 @@
 import folium
-
+from folium.map import Icon, Marker, Popup
 map = folium.Map(location = [42.3617864056339, -71.05954930389359], zoom_start=6, tiles="Stamen Terrain")
-map.add_child(folium.Marker())
 
+fg = folium.FeatureGroup(name="My Map")
+
+for coordinates in [[42.3617864056339, -71.05954930389359],[42.3503488063098, -71.10555066643477]]:
+    fg.add_child(folium.Marker(location=coordinates, popup="The best city in the world", icon=folium.Icon(color = 'green')))
+
+map.add_child(fg)
 map.save("Boston.html")

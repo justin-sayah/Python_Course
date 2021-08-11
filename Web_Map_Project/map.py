7ce8ff2fff50d4fa6adce811e2b179bb4b28c069
justin-sayah <jsayah@bu.edu>
2021-08-11 02:37:57 -0400

    pinned volcanoes

--- Web_Map_Project/map.py
+++ Web_Map_Project/map.py
@@ -1,11 +1,16 @@
-import folium
+import folium, pandas
 from folium.map import Icon, Marker, Popup
+
+data = pandas.read_csv("Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+
 map = folium.Map(location = [42.3617864056339, -71.05954930389359], zoom_start=6, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 
-for coordinates in [[42.3617864056339, -71.05954930389359],[42.3503488063098, -71.10555066643477]]:
-    fg.add_child(folium.Marker(location=coordinates, popup="The best city in the world", icon=folium.Icon(color = 'green')))
+for lt,ln in zip(lat,lon):
+    fg.add_child(folium.Marker(location=[lt,ln], popup="Temp", icon=folium.Icon(color = 'green')))
 
 map.add_child(fg)
-map.save("Boston.html")
+map.save("map.html")

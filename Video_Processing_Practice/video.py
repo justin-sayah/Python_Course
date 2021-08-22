5c658a830c7f2529f0304e57f8ec17685c37a636
justin-sayah <jsayah@bu.edu>
2021-08-22 15:19:32 -0400

    video capture implemented

---
+++ Video_Processing_Practice/video.py
@@ -0,0 +1,14 @@
+import cv2,time
+video = cv2.VideoCapture(0)
+
+while True:
+    check, frame = video.read()
+    cv2.imshow("Capturing", frame)
+    key = cv2.waitKey(1)
+
+    if key == ord('q'):
+        break
+
+video.release()
+
+cv2.destroyAllWindows()

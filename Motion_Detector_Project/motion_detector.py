8802d9abccc804c1dafb36ef0736abe62e4519e7
justin-sayah <jsayah@bu.edu>
2021-08-22 20:40:48 -0400

    added comments

---
+++ Motion_Detector_Project/motion_detector.py
@@ -0,0 +1,46 @@
+import cv2,time
+
+#For the first pass of the while loop, nothing happens as we need two frames
+# in order to comare deltas for motion 
+first_frame = None
+
+#Opens video feed from webcam
+video = cv2.VideoCapture(0)
+
+while True:
+    #Pulls the first frame from the webcam
+    check, frame = video.read()
+
+    #The check to see if the loop is in the first iteration
+    if first_frame is None:
+        first_frame = gray
+        continue
+
+    #Converts the frame to gray for easier analysis
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #Applies Gaussian Blur, which is necessary for motion detection
+    gray = cv2.GaussianBlur(gray, (21,21), 0)
+
+    #Gets a pixel by pixel difference between the current and previous frame, calculates
+    #contours and draws a box defined by the contour count
+    delta_frame = cv2.absdiff(first_frame, gray)
+    thresh_frame = cv2.threshold(delta_frame, 30,255, cv2.THRESH_BINARY)[1]
+    thresh_frame = cv2.dilate(thresh_frame, None, iterations=5)
+    (cnts,_) = cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in cnts:
+        if cv2.contourArea(contour) < 1000:
+            continue
+        (x,y,w,h) = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 3)
+
+    #Displays the frame w/ a drawn rectangle
+    cv2.imshow("Capturing", frame)
+    key = cv2.waitKey(1)
+
+    #Quits Program on 'Q' keypress
+    if key == ord('q'):
+        break
+#Cleanup
+video.release()
+cv2.destroyAllWindows()
